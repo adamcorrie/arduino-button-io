92af6e3dee69110c88ab05fe2280d8efaeb71570
Adam Corrie <apc@adamcorrie.com>
2019-07-27 17:33:14 +0100

    Create lean-button-io.ino

---
+++ lean-button-io/lean-button-io.ino
@@ -0,0 +1,28 @@
+/*
+ * Lean Button I/O
+ * 
+ * Connect an LED and a suitable current-limiting resistor
+ * between Pin 13 and GND. Or use the built-in LED. Up to you.
+ * 
+ * Connect a push button between 5V and Pin 7. Use a 10k Ohm 
+ * pull-down resistor.
+ * 
+ * The LED will light when the button is pressed.
+ * 
+ * Created by Adam Corrie on 27/07/2019
+ * Licensed under the GNU General Public License v3.0
+ * 
+ */
+
+#define buttonPin 7 // define buttonPin as Pin 7
+#define ledPin 13 // define ledPin as Pin 13
+
+void setup() {
+  pinMode(buttonPin, INPUT); // configure buttonPin as an INPUT pin
+  pinMode(ledPin, OUTPUT); // configure ledPin as an OUTPUT pin
+}
+
+void loop() {
+  // send the button state directly to the LED
+  digitalWrite(ledPin, digitalRead(buttonPin));
+}
