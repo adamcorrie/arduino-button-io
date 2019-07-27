280a531041e24b8ecf57671af9b6c1458b54f6ee
Adam Corrie <apc@adamcorrie.com>
2019-07-27 17:27:08 +0100

    Create simple-button-io.ino
    Comments updated

---
+++ simple-button-io/simple-button-io.ino
@@ -0,0 +1,33 @@
+/*
+ * Simple Button I/O
+ * 
+ * Connect an LED and a suitable current-limiting resistor
+ * between Pin 13 and GND. Or use the built-in LED. Up to you.
+ * 
+ * Connect a push button between 5V and Pin 7. Use a 10k Ohm 
+ * pull-down resistor.
+ * 
+ * The LED will light when the button is pressed.
+ * 
+ * Created by Adam Corrie on 27/07/2019
+ * Licensed under the GNU General Public License v3.0
+ * 
+ */
+
+#define buttonPin 7 // define buttonPin as Pin 7
+#define ledPin 13 // define ledPin as Pin 13
+
+void setup() {
+  pinMode(buttonPin, INPUT); // configure buttonPin as an INPUT pin
+  pinMode(ledPin, OUTPUT); // configure ledPin as an OUTPUT pin
+}
+
+void loop() {
+  // check to see if the button is pressed
+  if (digitalRead(buttonPin) == HIGH) {
+    digitalWrite(ledPin, HIGH); // turn on the LED
+  }
+  else {
+    digitalWrite(ledPin, LOW); // turn off the LED
+  }
+}
